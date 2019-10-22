666b0b0f912493da74fdf40fb35315d6f40e7c4f
Gregoire Clement <gregoire.clement@epfl.ch>
2019-10-23 03:17:35 +0900

    move out examples

--- examples/random_game.py
+++ examples/random_game.py
@@ -2,7 +2,7 @@ from jass.agents.random_agent import RandomAgent
 from jass.logic.game import Game
 
 if __name__ == '__main__':
-    humans = [RandomAgent() for _ in range(4)]
+    random_agents = [RandomAgent() for _ in range(4)]
     names = ['Jean', 'Anne', 'Luc', 'Sophie']
 
-    Game(names, humans).start()
+    Game(names, random_agents).start()
